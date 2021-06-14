cd0fcd1e8b80051e22a64d002fa64d11f6f7672f
JillKlatt <klatt.jill@gmail.com>
2021-06-14 14:03:23 -0400

    commit tree 0b0c166572a7d88c9363570f5ea1194672f2fcea

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  //debugger
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, index) => (
+          <div key={index}>
+            <h3>Name: {movie.title}</h3> 
+            <p>{movie.time}</p>
+            <p>Genres:</p>
+            <ul>
+              {movie.genres.map((genre, index) =>(
+                 <li key={index}>{genre}</li>
+              ))}
+            </ul>
+            </div>
+  ))
+  }
     </div>
   );
 };
